7600664c8c401d01f7abaa0b53a8f925fa65669e
Dikson Santos <diksonfer@gmail.com>
2022-08-11 02:51:38 -0300

    add content to the Info component

--- src/Info.js
+++ src/Info.js
@@ -3,14 +3,49 @@ import "./Info.css";
 function Info() {
   return (
     <div className="Info">
+      <h2>Por que o Cebolinha fala assim?</h2>
       <p>
-        Por que o Cebolinha fala assim? O Cebolinha possui dislalia, uma
-        alteração da fala em que a pessoa não consegue articular e pronunciar
-        algumas palavras, principalmente quando possuem R ou L. No caso do
-        Cebolinha, ele não consegue pronunciar o "R" do meio das palavras,
-        pronunciando a letra L no lugar, uma característica marcante desse
-        personagem da Turma da Mônica.
+        O Cebolinha possui dislalia, uma alteração da fala em que a pessoa não
+        consegue articular e pronunciar algumas palavras, principalmente quando
+        possuem R ou L. No caso do Cebolinha, ele não consegue pronunciar o "R"
+        do meio das palavras, pronunciando a letra L no lugar, uma
+        característica marcante desse personagem da Turma da Mônica.
       </p>
+      <h2>Culiosidades</h2>
+      <ul>
+        <li>
+          Ao contrário do que muitos pensam, o Cebolinha não troca o R do final
+          das palavras.
+        </li>
+        <li>
+          Além disso, nos gibis, quando a letra R vem antes de um L nos gibis,
+          ela é substituída por um U. No entanto, essa substituição é bem rara!
+        </li>
+        <li>
+          Nas historinhas mais antigas, os diálogos do Cebolinha usavam aspas em
+          volta das palavras "eladas"
+        </li>
+        <li>
+          Após tratamento fonoaudiológico, o Cebolinha consegue falar
+          normalmente na Turma da Mônica Jovem – Mas ainda troca as letras
+          quando nervoso.
+        </li>
+        <li>
+          O problema do Cebolinha é só na fala, então ele escreve e pensa sem
+          trocar as letras.
+        </li>
+      </ul>
+      <h2>Refelências</h2>
+      <ul>
+        <li>
+          Matéria no Tua Saúde sobre dislalia: Dislalia: o que é, causas e
+          tratamento
+        </li>
+        <li>
+          Repositório no GitHub usado para calcular a conversão: Inútil
+          transcritor para o Cebolinha.
+        </li>
+      </ul>
     </div>
   );
 }
