38d8e1d6cf04cb5a6857b89e4c70cc510fe5466b
Dikson Santos <diksonfer@gmail.com>
2022-09-06 04:20:57 -0300

    add a reference to the original cebolinha package

--- src/Translation.js
+++ src/Translation.js
@@ -1,30 +1,7 @@
 import "./Translation.css";
-import "./Common.css";
 import staticData from "./static.json";
 import { useState } from "react";
-
-// From github.com/theuves-projects/cebolinha
-// TODO: use the original repository
-function cebolinha(text) {
-  return text
-    .replace(/r(?=(l|L))/g, "u")
-    .replace(/R(?=(l|L))/g, "U")
-    .replace(/r+(?!\b)/g, "l")
-    .replace(/R+(?!\b)/g, "L");
-}
-
-// function TranslationArea({}) {
-//   return (
-//     <textarea
-//         className="TranslationArea"
-//         // cols="30"
-//         // rows="20"
-//         spellCheck="false"
-//         value={enteredText}
-//         onChange={(e) => setEnteredText(e.target.value)}
-//       />
-//   )
-// }
+import cebolinha from "cebolinha";
 
 function TranslationBoxInput({ enteredText, setEnteredText }) {
   return (
---
+++ src/cebolinha.d.ts
@@ -0,0 +1,4 @@
+declare module "cebolinha";
+
+// This is a workaround for the fact that the cebolinha package doesn't have a typescript declaration file.
+// A better solution would be to add a typescript declaration file to the cebolinha package by contributing to the project - TODO...
